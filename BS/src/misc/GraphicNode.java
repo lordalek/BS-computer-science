f11e088dc27ce90193bb81f739a4c84be3ff6377
Aleksander Pettersen <alekspett@gmail.com>
2012-09-18 21:18:44 +0200

    MarkLeavesAndRoot added and make the node class more viable

--- BS/src/misc/GraphicNode.java
+++ BS/src/misc/GraphicNode.java
@@ -3,16 +3,22 @@ package misc;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 
 public class GraphicNode {
 	private int valueOfNode, sizeOfNode = 50;
 	private Ellipse2D shape;
 	private Color color;
+	private Point2D bottomPoint, topPoint, valuePosition;
 	
 	public GraphicNode(int valueOfNode, int posX, int posY){
 		this.valueOfNode = valueOfNode;
 		shape = new Ellipse2D.Double(posX, posY, sizeOfNode, sizeOfNode);
 		color = Color.GREEN;
+		 left = right = null;
+		 bottomPoint = new Point2D.Double(posX+25, posY+50);
+		 topPoint = new Point2D.Double(posX+25, posY);
+		 valuePosition = new Point2D.Double(posX+20, posY+30);
 	}
 	
 	public Ellipse2D getShape(){
@@ -24,6 +30,10 @@ public class GraphicNode {
 		return valueAsString;
 	}
 	
+	public Point2D getPositionOfValue(){
+		return valuePosition;
+	}
+	
 	public void checkIfPointIsInsideNode(Point point){
 		if(shape.contains(point)) {
 			System.out.println(shape.contains(point));
@@ -32,7 +42,32 @@ public class GraphicNode {
 		}
 	}
 	
+	public void changeColor(){
+		if(color == Color.GREEN) color = Color.YELLOW;
+		else color = Color.GREEN;
+	}
+	
+	public void changeColor(String specialPosition){
+		if(color == Color.GREEN) color = Color.RED;
+		else color = Color.GREEN;
+	}
+	
+	public Point2D getBottomPoint() {
+		return bottomPoint;
+	}
+
+	public Point2D getTopPoint() {
+		return topPoint;
+	}
+
 	public Color getColor(){
 		return color;
 	}
+	
+	public void setColor(Color color){
+		this.color = color;
+	}
+	
+	GraphicNode left; //Left Child
+	GraphicNode right; //Right Child
 }
